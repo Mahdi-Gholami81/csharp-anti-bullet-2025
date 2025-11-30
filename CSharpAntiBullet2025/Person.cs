4ba485c6d7b64a2fa4c722e053e52d25801f8d1d
Mahdi <mahdi.gholami1381@yahoo.com>
2025-11-30 11:04:58 +0330

    اولین کامیت – ۸ کلاس پایه + Program.cs

--- CSharpAntiBullet2025/Person.cs
+++ CSharpAntiBullet2025/Person.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CSharpAntiBullet2025
 {
-    internal class Person
+    public class Person
     {
+        private string FirstName;
+        private string LastName;
+        private int ID;
+        private string PhoneNumber;
+        private string FatherName;
+        private int Age;
+        public Person(string fname, int age)
+        {
+            fname = FirstName;
+            age = Age;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"hello!/n my name is meysam naderi and have 18 years old learning visual studio and happy!");
+        }
     }
+    
 }
--- CSharpAntiBullet2025/Program.cs
+++ CSharpAntiBullet2025/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// تست سریع همه کلاس‌ها
+using CSharpAntiBullet2025;
+
+var p = new Person("مهدی", 30);
+p.Introduce();
+
+var acc = new BankAccount();
+acc.Deposit(1000);
+acc.Withdraw(200);
+Console.WriteLine(acc.Balance);
+
+Console.WriteLine("سلام دنیا".WordCount());
+Console.WriteLine(17.IsPrime());
+
+var point = new Point3D(3, 4, 0);
+Console.WriteLine(point.DistanceFromOrigin());
