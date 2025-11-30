d10d46d9c0ada9413c4fd6750e07902372e4aa87
Mahdi <mahdi.gholami1381@yahoo.com>
2025-11-30 11:03:36 +0330

    اولین کامیت – ۸ کلاس پایه + Program.cs

---
+++ CSharpAntiBullet2025/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAntiBullet2025
+{
+    internal class BankAccount
+    {
+        private int BankID;
+        private string AccountBalance=0;
+
+        public void Balance()
+        {
+            AccountBalance = value;
+        }
+        public string Deposit(string Value)
+        {
+            AccountBalance = Value;
+            Console.WriteLine($"you added {Value} to your balance successfully!");
+            return AccountBalance;
+        }
+
+        public string Withdraw (string value)
+        {
+            AccountBalance - value= AccountBalance;
+            Console.WriteLine($"you withdraw {value} from your balance successfully!");
+            return AccountBalance;
+        }
+    }
+}
